a578ee7f4d3360fb42681d1417a1e96ec1d3b899
Михаил Гольдфарб <goldfarbm@mail.ru>
2024-10-22 17:18:22 +0500

    finish

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
+app_name = 'pages'
 urlpatterns = [
-
+    path('pages/about/', views.about, name='about'),
+    path('pages/rules/', views.rules, name='rules')
 ]
